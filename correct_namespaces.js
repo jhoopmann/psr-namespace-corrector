8307bb845eebf201b93573dab9aa6074c26783f3
Jonas Hoopmann <jhoopmann@sportimport.de>
2019-11-25 22:25:33 +0100

    repl fix

--- correct_namespaces.js
+++ correct_namespaces.js
@@ -65,8 +65,8 @@ function _readDir(
                 ) {
                     _readDir(
                         root, 
-                        new RegExp(match.replace('namespace', 'use').replace(';',''),'gm'),
-                        replace.replace('namespace', 'use').replace(';','') + '\\' + dirent.name.replace('.php', '')
+                        new RegExp(match.replace('namespace', 'use').replace(';','')+'\\','gm'),
+                        replace.replace('namespace', 'use').replace(';','')+'\\'
                     );
                 }
             }   
